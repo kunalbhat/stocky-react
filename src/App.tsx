ddba0ee3b77c1463119b1d0432640dcb1a2bd4fe
Kunal Bhat <kunalbhat@gmail.com>
2023-10-07 07:22:25 -0500

    hit endpoint for symbol data

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,43 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
+import "./App.css";
 
 function App() {
+  const POLYGON_API_KEY = process.env.REACT_APP_POLYGON_API_KEY;
+
+  const [quoteData, setQuoteData] = useState();
+
+  async function fetchQuote() {
+    const BASE_URL = "https://api.polygon.io/v2/aggs/ticker/";
+    let SYMBOL = "PYPL";
+
+    let FETCH_URL = `${BASE_URL}${SYMBOL}/prev?adjusted=true&apiKey=${POLYGON_API_KEY}`;
+
+    const response = await fetch(FETCH_URL);
+    const quote_data = await response.json();
+
+    console.log("Response: ", quote_data);
+    setQuoteData(quote_data["results"]);
+  }
+
+  useEffect(() => {
+    fetchQuote();
+  }, []);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header className="app-header">
+        <h1 className="logo">Stocky</h1>
       </header>
+      <section>
+        {quoteData && (
+          <ul className="stock-list">
+            <li className="stock-list-row">
+              <span className="symbol">{quoteData[0]["T"]}</span>
+              <span className="close-price">{quoteData[0]["c"]}</span>
+            </li>
+          </ul>
+        )}
+      </section>
     </div>
   );
 }
